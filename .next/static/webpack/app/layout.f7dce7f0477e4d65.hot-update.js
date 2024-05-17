42ac0c279c6ab017c32b9ea04a3971bcea29c6eb
DeathWarlock <ylsmerino@gmail.com>
2024-05-17 15:34:59 +0100

    week 3

---
+++ .next/static/webpack/app/layout.f7dce7f0477e4d65.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/layout",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CBYU%5CBYU%20Idaho%5CWDD%20430%20-%20Web%20Full-Stack%20Development%5CWeek%2002%5CNextJs%5Cnextjs-dashboard%5Capp%5Cui%5Cglobal.css&server=false!":
+/*!*********************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CBYU%5CBYU%20Idaho%5CWDD%20430%20-%20Web%20Full-Stack%20Development%5CWeek%2002%5CNextJs%5Cnextjs-dashboard%5Capp%5Cui%5Cglobal.css&server=false! ***!
+  \*********************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/ui/global.css */ \"(app-pages-browser)/./app/ui/global.css\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1EJTNBJTVDQllVJTVDQllVJTIwSWRhaG8lNUNXREQlMjA0MzAlMjAtJTIwV2ViJTIwRnVsbC1TdGFjayUyMERldmVsb3BtZW50JTVDV2VlayUyMDAyJTVDTmV4dEpzJTVDbmV4dGpzLWRhc2hib2FyZCU1Q2FwcCU1Q3VpJTVDZ2xvYmFsLmNzcyZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/NDVhZiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkQ6XFxcXEJZVVxcXFxCWVUgSWRhaG9cXFxcV0REIDQzMCAtIFdlYiBGdWxsLVN0YWNrIERldmVsb3BtZW50XFxcXFdlZWsgMDJcXFxcTmV4dEpzXFxcXG5leHRqcy1kYXNoYm9hcmRcXFxcYXBwXFxcXHVpXFxcXGdsb2JhbC5jc3NcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CBYU%5CBYU%20Idaho%5CWDD%20430%20-%20Web%20Full-Stack%20Development%5CWeek%2002%5CNextJs%5Cnextjs-dashboard%5Capp%5Cui%5Cglobal.css&server=false!\n"));
+
+/***/ })
+
+});
